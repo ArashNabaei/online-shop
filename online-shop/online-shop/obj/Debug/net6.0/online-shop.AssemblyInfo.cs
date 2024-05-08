c438f15c6359e8902dfe74c3956a0ccbf80a1627
AliTaleshi <alitys083@gmail.com>
2024-05-08 21:29:09 +0330

    Add category class

--- online-shop/online-shop/obj/Debug/net6.0/online-shop.AssemblyInfo.cs
+++ online-shop/online-shop/obj/Debug/net6.0/online-shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13550490063fda7e606976f67cf37ce4a91c297b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f53a88cdae205724fae4e155137706bdf90615b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
