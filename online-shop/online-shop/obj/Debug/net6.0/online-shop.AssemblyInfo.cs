f53a88cdae205724fae4e155137706bdf90615b8
AliTaleshi <alitys083@gmail.com>
2024-05-08 21:17:56 +0330

    Add assembly classes

--- online-shop/online-shop/obj/Debug/net6.0/online-shop.AssemblyInfo.cs
+++ online-shop/online-shop/obj/Debug/net6.0/online-shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8123fdff20b8797b7099ebd1e8b734b575fa3e52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13550490063fda7e606976f67cf37ce4a91c297b")]
 [assembly: System.Reflection.AssemblyProductAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
