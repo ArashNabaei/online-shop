13550490063fda7e606976f67cf37ce4a91c297b
AliTaleshi <alitys083@gmail.com>
2024-05-08 21:14:27 +0330

    Add product class

---
+++ online-shop/Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        [ForeignKey("Category")]
+        public Guid CategoryId { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
--- online-shop/online-shop/obj/Debug/net6.0/online-shop.AssemblyInfo.cs
+++ online-shop/online-shop/obj/Debug/net6.0/online-shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8123fdff20b8797b7099ebd1e8b734b575fa3e52")]
 [assembly: System.Reflection.AssemblyProductAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
