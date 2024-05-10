c1a218cde5a7dfab75d176c6d2b42c2b5cbea847
AliTaleshi <alitys083@gmail.com>
2024-05-10 23:15:29 +0330

    Add Order class

---
+++ online-shop/Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public enum OredrStatus
+        {
+            Pending,
+            Shipped,
+            Delivered,
+            Cancelled
+        }
+        public OrderStatus Status { get; set; }
+        public double TotalPrice { get; set; }
+        public string ShippingAddress { get; set; }
+    }
+}
