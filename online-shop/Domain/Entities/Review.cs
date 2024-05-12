bd709f898b2d928f6bcf4b7748767ddb7ae9844d
AliTaleshi <alitys083@gmail.com>
2024-05-12 19:36:31 +0330

    Add review class

---
+++ online-shop/Domain/Entities/Review.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        [ForeignKey("User")]
+        public Guid UserId { get; set; }
+        [ForeignKey("Product")]
+        public Guid ProductId { get; set; }
+        public float Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
