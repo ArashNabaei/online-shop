81cf04287cf1aa52fd20adc0e3882d1bd3a57cd3
AliTaleshi <alitys083@gmail.com>
2024-05-12 19:33:49 +0330

    Add cart class

---
+++ online-shop/Domain/Entities/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        [ForeignKey("User")]
+        public Guid UserId { get; set; }
+        [ForeignKey("Product")]
+        public Guid ProductId { get; set; }
+        [ForeignKey("Order")]
+        public Guid OrderId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime AddedTime { get; set; }
+    }
+}
