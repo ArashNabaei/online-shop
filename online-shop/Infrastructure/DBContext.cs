6edbd80e6204455ef6b480292f5c1ed5fc643dbe
AliTaleshi <alitys083@gmail.com>
2024-05-13 16:04:54 +0330

    Add DBContext class, sql server & entitty framework packages

---
+++ online-shop/Infrastructure/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+        // DbSet properties for your entities
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
--- online-shop/online-shop/obj/Debug/net6.0/online-shop.AssemblyInfo.cs
+++ online-shop/online-shop/obj/Debug/net6.0/online-shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd709f898b2d928f6bcf4b7748767ddb7ae9844d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357a9ec9772165eb702d8c5ed18341b12742bf0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("online-shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
