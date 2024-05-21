bfd7ac1c0f5e82c7158973de5f86d50eb15e0185
AliTaleshi <alitys083@gmail.com>
2024-05-21 14:50:26 +0330

    Add CartItemRepository class

---
+++ online-shop/Infrastructure/Repositories/CartItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Repositories;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class CartItemRepository : Repository<CartItemRepository>, ICartItemRepository
+    {
+        public CartItemRepository(DBContext dbContext) : base(dbContext) { }
+    }
+}
