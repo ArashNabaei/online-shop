dbcd5603a35fda6ce8d2cd3e24ad0fd3c55fe0ad
AliTaleshi <alitys083@gmail.com>
2024-05-21 14:41:05 +0330

    Add RoleRepository class

---
+++ online-shop/Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class RoleRepository : Repository<Role>, IRoleRepository
+    {
+        public RoleRepository(DBContext dBContext) : base(dBContext) { }
+    }
+}
