5b436c3c30094a98d3028875388adaaa6b7a8277
AliTaleshi <alitys083@gmail.com>
2024-05-21 14:45:26 +0330

    Add OrderRepository class

---
+++ online-shop/Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(DBContext dbContext) : base(dbContext) { }
+    }
+}
